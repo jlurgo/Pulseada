56e195e8d712940df719843d3f19a7507c66329d
unknown <clurgo@mds-1163.sds_domain_1.local>
2013-09-06 20:06:08 -0300

    4_ver_flechas_fuerzas: refectorizado para usar los vectores de paperjs

--- Bolita.js
+++ Bolita.js
@@ -3,51 +3,46 @@ var Bolita = function(opt){
     this.start();  
 };
 
-Bolita.prototype.start = function(){       
+Bolita.prototype.start = function () {
     this.circulo = new paper.Path.Circle(paper.project.view.center, 30);
     this.circulo.fillColor = 'red';
-    this.masa = 0.01;
+    this.masa = 10;
     this.portal = new NodoPortalBidi();
     NodoRouter.instancia.conectarBidireccionalmenteCon(this.portal);
-    
-    this.portal.pedirMensajes(  new FiltroXClaveValor("tipoDeMensaje", "vortex.pulseada.fuerza"),
-                                function(mensaje){_this.fuerzaRecibida(mensaje);});
-    this.velocidad = {x:0, y:0};
-    this.fuerza = {x:0, y:0};
-    this.posicion = {x:paper.project.view.center.x, y:paper.project.view.center.y};
-    
+
+    this.portal.pedirMensajes(new FiltroXClaveValor("tipoDeMensaje", "vortex.pulseada.fuerza"),
+                                function (mensaje) { _this.fuerzaRecibida(mensaje); });
+
+    this.velocidad = new paper.Point(0, 0);
+    this.fuerza = new paper.Point(0, 0);
+    this.posicion = paper.project.view.center.clone();
+
     this.periodoDeMuestreo_ms = 200;
-    this.periodoDeMuestreo_s = this.periodoDeMuestreo_ms/1000;
-    
+    this.periodoDeMuestreo_s = this.periodoDeMuestreo_ms / 1000;
+
     var _this = this;
-    paper.view.onFrame  = function(event) {
-        _this.circulo.position = new paper.Point(_this.posicion.x, _this.posicion.y);
+    paper.view.onFrame = function (event) {
+        _this.circulo.position = _this.posicion;
     };
-    setInterval(function(){
+    setInterval(function () {
         _this.actualizarPosicion();
     }, this.periodoDeMuestreo);
-    
+
     this.vista_fuerza = new paper.Path();
     this.vista_fuerza.strokeColor = 'blue';
     this.vista_fuerza.strokeWidth = 5;
 };
 
-Bolita.prototype.fuerzaRecibida = function(fuerza){   
-    this.fuerza.x = fuerza.x;
-    this.fuerza.y = fuerza.y;    
+Bolita.prototype.fuerzaRecibida = function (fuerza) {
+    this.fuerza = new paper.Point(fuerza.x, fuerza.y);  
 };
 
-Bolita.prototype.actualizarPosicion = function(fuerza){   
-    this.velocidad.x += this.fuerza.x * this.periodoDeMuestreo_s / this.masa;
-    this.velocidad.y += this.fuerza.y * this.periodoDeMuestreo_s / this.masa;
-    this.velocidad.x -= this.velocidad.x * 0.01;
-    this.velocidad.y -= this.velocidad.y * 0.01;
-    
-    this.posicion.x += this.velocidad.x * this.periodoDeMuestreo_s/1000;
-    this.posicion.y += this.velocidad.y * this.periodoDeMuestreo_s/1000;
-    
+Bolita.prototype.actualizarPosicion = function (fuerza) {
+    this.velocidad = this.velocidad.add(this.fuerza.multiply(this.periodoDeMuestreo_s * (1 / this.masa)));
+    this.velocidad = this.velocidad.multiply(0.99);
+    this.posicion = this.posicion.add(this.velocidad.multiply(this.periodoDeMuestreo_s));
     this.vista_fuerza.segments = [
 	   [this.circulo.position],
-	   [this.circulo.position.add([this.fuerza.x*100, this.fuerza.y*100])]
+	   [this.circulo.position.add(this.fuerza.multiply(50))]
     ];
 };
